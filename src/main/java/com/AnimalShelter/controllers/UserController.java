bdb4bf301f3f569ff0e1a701bf54e2e14e4c0b58
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-08-27 10:56:38 +0200

    fix: rename api endpoints to fit convention

--- src/main/java/com/AnimalShelter/controllers/UserController.java
+++ src/main/java/com/AnimalShelter/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/v1/users")
 @CrossOrigin(origins = "*")
 
 public class UserController {
