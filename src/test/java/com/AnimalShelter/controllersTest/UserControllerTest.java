8559561932a38d5551fc4fe8bc8c419ad514df6c
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-08-27 12:37:31 +0200

    update: add test common variables for extendwith and some injections

--- src/test/java/com/AnimalShelter/controllersTest/UserControllerTest.java
+++ src/test/java/com/AnimalShelter/controllersTest/UserControllerTest.java
@@ -2,20 +2,24 @@ package com.AnimalShelter.controllersTest;
 
 import com.AnimalShelter.controllers.UserController;
 import com.AnimalShelter.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
-
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private UserService userService;
+    private ObjectMapper objectMapper;
 
     @InjectMocks
     private UserController userController;
--- src/test/java/com/AnimalShelter/servicesTest/UserServiceTest.java
+++ src/test/java/com/AnimalShelter/servicesTest/UserServiceTest.java
@@ -5,9 +5,12 @@ import com.AnimalShelter.models.User;
 import com.AnimalShelter.repositories.IPetRepository;
 import com.AnimalShelter.repositories.IUserRepository;
 import com.AnimalShelter.services.UserService;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     @Mock
     private IUserRepository iUserRepository;
