25e662df4e64d82981a0b35007fd3bbb4d2a2680
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-08-27 11:00:26 +0200

    feat: add delete method for user

--- src/main/java/com/AnimalShelter/controllers/UserController.java
+++ src/main/java/com/AnimalShelter/controllers/UserController.java
@@ -2,10 +2,7 @@ package com.AnimalShelter.controllers;
 
 import com.AnimalShelter.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -15,8 +12,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @DeleteMapping (path= "/")
-    public void deleteUserById(Long id){
+    @DeleteMapping (path= "/user/{id}")
+    public void deleteUserById(@PathVariable Long id){
         userService.deleteUser(id);
     }
 
