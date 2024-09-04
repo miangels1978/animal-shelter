ed14a8e9cee535866e7b8a436d13427b5557d44e
miangels1978 <einsteinvie@gmail.com>
2024-09-04 16:35:20 +0200

    update

--- src/main/java/com/AnimalShelter/controllers/AuthController.java
+++ src/main/java/com/AnimalShelter/controllers/AuthController.java
@@ -1,30 +1,30 @@
-//package com.AnimalShelter.controllers;
-//import com.AnimalShelter.dtos.request.LoginRequest;
-//import com.AnimalShelter.dtos.request.RegisterRequest;
-//import com.AnimalShelter.dtos.response.AuthResponse;
-//import com.AnimalShelter.services.AuthService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//@RequiredArgsConstructor
-//@CrossOrigin(origins = "*")
-//public class AuthController {
-//
-//    private final AuthService authService;
-//
-//    @PostMapping(value = "login")
-//    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
-//    {
-//        return ResponseEntity.ok(authService.login(request));
-//    }
-//
-//    @PostMapping(value = "register")
-//    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
-//    {
-//        return ResponseEntity.ok(authService.register(request));
-//    }
-//
-//}
+package com.AnimalShelter.controllers;
+import com.AnimalShelter.dtos.request.LoginRequest;
+import com.AnimalShelter.dtos.request.RegisterRequest;
+import com.AnimalShelter.dtos.response.AuthResponse;
+import com.AnimalShelter.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
+    {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
+    {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+}
